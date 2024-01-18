cac97a383e6a8cc879051118fdd0b577d1df684d
Aditya <as2397@hw.ac.uk>
2024-01-18 23:12:12 +0400

    Added more comments

--- Lecture 1/quadraticSolver.c
+++ Lecture 1/quadraticSolver.c
@@ -24,10 +24,14 @@ int main()
     printf("Enter the constant: ");
     scanf("%d", &constant);
 
+    // Just something to display the quadratic equation the user has typed
     printf("Your equation is %dx^2 + %dx + %d \n", coeff1, coeff2, constant);
 
+    // Use the quadratic formula to solve the given equation, and do two cases, one for positive roots and the other for negative roots
+    // It is stored in doubles because pow function of math module returns the answer as a double, pow is a function used for exponents
     double posanswer = (-(coeff2) + pow((pow(coeff2, 2) - (4 * coeff1 * constant)), 0.5)) / (2 * coeff1);
     double neganswer = (-(coeff2)-pow((pow(coeff2, 2) - (4 * coeff1 * constant)), 0.5)) / (2 * coeff1);
 
+    // Display the positive and negative answers in the placehodlers %lf which stands for long floating point, the .3 before lf is used to specify how many digits after the decimal point is required
     printf("The positive root is: %.3lf and %.3lf \n", posanswer, neganswer);
 }
